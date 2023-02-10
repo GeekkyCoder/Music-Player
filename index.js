b378cca332f7d35cebd5cc3ae20f53b37ce3b1a2
Faraz <farazatgithub@gmail.com>
2023-02-10 17:24:59 +0500

    added few songs

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 // Elements
 import { secondsToHms, convertHMS } from "./utils.js";
 
-const sliderElm = document.querySelector("#music-slider")
-const prevEl = document.querySelector("#prev")
-const nextEl = document.querySelector("#next")
-const masterPlay = document.querySelector("#master-play")
-const startEl = document.querySelector("#start")
-const endEl = document.querySelector("#end")
-const cover = document.querySelector("#cover")
-
+const sliderElm = document.querySelector("#music-slider");
+const prevEl = document.querySelector("#prev");
+const nextEl = document.querySelector("#next");
+const masterPlay = document.querySelector("#master-play");
+const startEl = document.querySelector("#start");
+const endEl = document.querySelector("#end");
+const cover = document.querySelector("#cover");
+const songEl = document.querySelector(".song");
 // glob vaiables
 
 let audioIndex = 0;
 
 const audios = [
   {
-    songName: "song1.mp3",
+    songName: "Believer.mp3",
     src: "./songs",
-    coverPath: "./covers/cover1.jpg",
+    coverPath: "./covers/believer.png",
   },
   {
-    songName: "song2.mp3",
+    songName: "perfect.mp3",
     src: "./songs",
-    coverPath: "./covers/cover2.jpeg",
+    coverPath: "./covers/perfect.jpg",
   },
   {
-    songName: "song3.mp3",
+    songName: "demons.mp3",
     src: "./songs",
-    coverPath: "./covers/cover3.jpg",
+    coverPath: "./covers/demon.jpg",
   },
   {
     songName: "song4.mp3",
@@ -36,61 +36,61 @@ const audios = [
   },
 ];
 
-let audioElement = new Audio("./songs/song2.mp3")
+let audioElement = new Audio("./songs/Believer.mp3");
 
 const handleMasterPlayClick = () => {
   if (audioElement.paused || audioElement.currentTime <= 0) {
     audioElement.play();
-    masterPlay.classList.remove("fa-circle-play")
-    masterPlay.classList.add("fa-circle-pause")
+    masterPlay.classList.remove("fa-circle-play");
+    masterPlay.classList.add("fa-circle-pause");
   } else {
     audioElement.pause();
-    masterPlay.classList.add("fa-circle-play")
-    masterPlay.classList.remove("fa-circle-pause")
+    masterPlay.classList.add("fa-circle-play");
+    masterPlay.classList.remove("fa-circle-pause");
   }
 };
 
 const handleSlider = () => {
-  audioElement.currentTime = (sliderElm.value * audioElement.duration) / 100
+  audioElement.currentTime = (sliderElm.value * audioElement.duration) / 100;
 };
 
 const handleTimeUpdate = () => {
   let progress = parseInt(
     (audioElement.currentTime / audioElement.duration) * 100
   );
-  endEl.textContent = convertHMS(audioElement.duration)
-  startEl.textContent = secondsToHms(progress)
-  sliderElm.value = progress
+  endEl.textContent = convertHMS(audioElement.duration);
+  startEl.textContent = secondsToHms(progress);
+  sliderElm.value = progress;
 };
 
 const handleNextAudio = () => {
-  audioIndex++
+  audioIndex++;
   if (audioIndex > audios.length) {
-    alert("end of songs going back to first song")
+    alert("end of songs going back to first song");
     audioIndex = 0;
-    return
+    return;
   }
+  songEl.textContent = audios[audioIndex].songName.split(".")[0];
   cover.src = audios[audioIndex].coverPath;
-  audioElement.src = `${audios[audioIndex].src}/${audios[audioIndex].songName}`
+  audioElement.src = `${audios[audioIndex].src}/${audios[audioIndex].songName}`;
   audioElement.currentTime = 0;
   audioElement.play();
 };
 
 const handlePrevAudio = () => {
-  audioIndex--
-  if(audioIndex == -1){
-     alert("limit")
-     return
+  audioIndex--;
+  if (audioIndex == -1) {
+    return;
   }
-
-  cover.src = audios[audioIndex].coverPath
-  audioElement.src = `${audios[audioIndex].src}/${audios[audioIndex].songName}`
+  songEl.textContent = audios[audioIndex].songName.split(".")[0];
+  cover.src = audios[audioIndex].coverPath;
+  audioElement.src = `${audios[audioIndex].src}/${audios[audioIndex].songName}`;
   audioElement.currentTime = 0;
   audioElement.play();
-}
+};
 
 masterPlay.addEventListener("click", handleMasterPlayClick);
 audioElement.addEventListener("timeupdate", handleTimeUpdate);
 sliderElm.addEventListener("change", handleSlider);
 nextEl.addEventListener("click", handleNextAudio);
-prevEl.addEventListener("click", handlePrevAudio)
+prevEl.addEventListener("click", handlePrevAudio);
